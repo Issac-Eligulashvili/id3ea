ac5568324730060d53a0974ea9b44302358ed49a
mabby <mabby@LAPTOP-9JTHR270>
2026-04-10 11:13:04 -0400

    im pushing as hard as i can

--- mainScript.m
+++ mainScript.m
@@ -1,8 +1,1 @@
-array_paths = ["Wet Sounds_Balloon Pop Stairwell 2.mp3", "Wet Sounds_Balloon Pop Stairwell.mp3", "Wet Sounds_Shoe Smack in Stairwell 1.mp3", "Wet Sounds_Shoe Smack in Stairwell 2.mp3"];
-
-for i = 1:length(array_paths)
-    [audio_samples,fs]=audioread(array_paths(i));
-
-    %rms_value=
-end
-
+array_paths = ["Wet Sounds\Wet Sounds_Balloon Pop Stairwell 2.mp3", "Wet Sounds\Wet Sounds_Balloon Pop Stairwell.mp3", "Wet Sounds\Wet Sounds_Shoe Smack in Stairwell 1.mp3", "Wet Sounds\Wet Sounds_Shoe Smack in Stairwell 2.mp3"];
