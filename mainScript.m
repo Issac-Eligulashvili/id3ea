c5e9b45b5395c2d36ba8212026976b2808a11f56
mabby <mabby@LAPTOP-9JTHR270>
2026-04-10 11:33:49 -0400

    pushed

--- mainScript.m
+++ mainScript.m
@@ -1,4 +1,7 @@
 array_paths = ["Wet Sounds\Wet Sounds_Balloon Pop Stairwell 2.mp3", "Wet Sounds\Wet Sounds_Balloon Pop Stairwell.mp3", "Wet Sounds\Wet Sounds_Shoe Smack in Stairwell 1.mp3", "Wet Sounds\Wet Sounds_Shoe Smack in Stairwell 2.mp3"];
 
 for i=1:length(array_paths)
-    [audio_samples,fs]=audioread(array_path(i));
+    [audio_samples,fs]=audioread(array_paths(i));
+
+    audio_clipped=audio_samples(1125:end,:);
+end
