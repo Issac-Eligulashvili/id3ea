d1f0e359e2affe8b67b94850e8eefdd522f09e91
mabby <mabby@LAPTOP-9JTHR270>
2026-04-10 11:05:59 -0400

    push harder

--- mainScript.m
+++ mainScript.m
@@ -1,1 +1,7 @@
-array_paths = ["Wet Sounds_Balloon Pop Stairwell 2.mp3", "Wet Sounds_Balloon Pop Stairwell.mp3", "Wet Sounds_Shoe Smack in Stairwell 1.mp3", "Wet Sounds_Shoe Smack in Stairwell 2.mp3"]
+array_paths = ["Wet Sounds_Balloon Pop Stairwell 2.mp3", "Wet Sounds_Balloon Pop Stairwell.mp3", "Wet Sounds_Shoe Smack in Stairwell 1.mp3", "Wet Sounds_Shoe Smack in Stairwell 2.mp3"];
+
+for i = 1:length(array_paths)
+    [audio_samples,fs]=audioread(array_paths(i));
+
+    rms_value=
+
