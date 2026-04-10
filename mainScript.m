53ba7e74ab334e4d54bbd01ab5a33875e7a38581
mabby <mabby@LAPTOP-9JTHR270>
2026-04-10 11:25:39 -0400

    pushing hard af

--- mainScript.m
+++ mainScript.m
@@ -1,1 +1,4 @@
 array_paths = ["Wet Sounds\Wet Sounds_Balloon Pop Stairwell 2.mp3", "Wet Sounds\Wet Sounds_Balloon Pop Stairwell.mp3", "Wet Sounds\Wet Sounds_Shoe Smack in Stairwell 1.mp3", "Wet Sounds\Wet Sounds_Shoe Smack in Stairwell 2.mp3"];
+
+for i=1:length(array_paths)
+    [audio_samples,fs]=audioread(array_path(i));
